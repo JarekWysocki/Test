4762c2f60ee245448fe400b258d19e0328e33427
Jarek Wysocki <jarwys@wp.pl>
2019-05-06 12:06:09 +0200

    Updates

---
+++ precache-manifest.717dd312c7e878472c4101d1c841fc3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d171a08e4f93b382bc30c41603c1cb13",
+    "url": "/ToDoReact/index.html"
+  },
+  {
+    "revision": "edd4d3c01c905908ae5f",
+    "url": "/ToDoReact/static/css/main.b604ac72.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ToDoReact/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "edd4d3c01c905908ae5f",
+    "url": "/ToDoReact/static/js/main.11c18e99.chunk.js"
+  },
+  {
+    "revision": "8f63ebf90f1c7e89a403",
+    "url": "/ToDoReact/static/js/runtime~main.cf6af53c.js"
+  }
+]);
